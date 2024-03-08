27df3acb961410c4bb7aafa564f83b90b0e60ad7
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-08 17:06:26 +0100

    Publish 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import '@project-chip/matter-node.js';
-
 import { Matterbridge } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
-import { EveDoorPlatform } from './platform.js';
+import { EveMotionPlatform } from './platform.js';
 
 /**
  * This is the standard interface for MatterBridge plugins.
@@ -11,10 +9,5 @@ import { EveDoorPlatform } from './platform.js';
  * @param matterbridge - An instance of MatterBridge
  */
 export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLogger) {
-  log.info('Matterbridge eve door with history plugin is loading...');
-
-  const platform = new EveDoorPlatform(matterbridge, log);
-
-  log.info('Matterbridge eve door with history plugin initialized successfully!');
-  return platform;
+  return new EveMotionPlatform(matterbridge, log);
 }
--- src/platform.ts
+++ src/platform.ts
@@ -1,12 +1,9 @@
-import '@project-chip/matter-node.js';
-import { DeviceTypes, logEndpoint } from '@project-chip/matter-node.js/device';
-import { BooleanState } from '@project-chip/matter-node.js/cluster';
+import { DeviceTypes, logEndpoint, IlluminanceMeasurement, OccupancySensing } from 'matterbridge';
 
-import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform } from 'matterbridge';
-import { MatterHistory } from 'matterbridge-history';
+import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform, MatterHistory } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 
-export class EveDoorPlatform extends MatterbridgeAccessoryPlatform {
+export class EveMotionPlatform extends MatterbridgeAccessoryPlatform {
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
   }
@@ -14,35 +11,41 @@ export class EveDoorPlatform extends MatterbridgeAccessoryPlatform {
   override async onStart(reason?: string) {
     this.log.info('onStart called with reason:', reason ?? 'none');
 
-    const history = new MatterHistory(this.log, 'Eve door');
+    const history = new MatterHistory(this.log, 'Eve motion', { filePath: this.matterbridge.matterbridgeDirectory });
 
-    const door = new MatterbridgeDevice(DeviceTypes.CONTACT_SENSOR);
-    door.createDefaultIdentifyClusterServer();
-    door.createDefaultBasicInformationClusterServer('Eve door', '0x88030475', 4874, 'Eve Systems', 77, 'Eve Door 20EBN9901', 1144, '1.2.8');
-    door.createDefaultPowerSourceReplaceableBatteryClusterServer(75);
-    door.createDefaultBooleanStateClusterServer(true);
-    door.createDoorEveHistoryClusterServer(history, this.log);
-    history.autoPilot(door);
-    this.registerDevice(door);
+    const motion = new MatterbridgeDevice(DeviceTypes.OCCUPANCY_SENSOR);
+    motion.createDefaultIdentifyClusterServer();
+    motion.createDefaultBasicInformationClusterServer('Eve motion', '0x85483499', 4874, 'Eve Systems', 89, 'Eve Motion 20EBY9901', 6650, '3.2.1');
+    motion.createDefaultOccupancySensingClusterServer();
+    motion.addDeviceType(DeviceTypes.LIGHT_SENSOR);
+    motion.createDefaultIlluminanceMeasurementClusterServer();
+    motion.createDefaultPowerSourceReplaceableBatteryClusterServer();
+    motion.createDefaultPowerSourceConfigurationClusterServer(1);
+    motion.createMotionEveHistoryClusterServer(history, this.log);
+    history.autoPilot(motion);
 
-    door.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
+    await this.registerDevice(motion);
+
+    motion.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
       this.log.warn(`Command identify called identifyTime:${identifyTime}`);
-      logEndpoint(door);
+      logEndpoint(motion);
       history.logHistory(false);
     });
 
     setInterval(
       () => {
-        let contact = door.getClusterServerById(BooleanState.Cluster.id)?.getStateValueAttribute();
-        contact = !contact;
-        door.getClusterServerById(BooleanState.Cluster.id)?.setStateValueAttribute(contact);
-        door.getClusterServerById(BooleanState.Cluster.id)?.triggerStateChangeEvent({ stateValue: contact });
-        if (contact === false) history.addToTimesOpened();
+        const occupancy = motion.getClusterServerById(OccupancySensing.Cluster.id)?.getOccupancyAttribute();
+        if (!occupancy) return;
+        occupancy.occupied = !occupancy.occupied;
+        const lux = history.getFakeLevel(0, 1000, 0);
+        motion.getClusterServerById(OccupancySensing.Cluster.id)?.setOccupancyAttribute(occupancy);
+        motion.getClusterServerById(IlluminanceMeasurement.Cluster.id)?.setMeasuredValueAttribute(Math.round(Math.max(Math.min(10000 * Math.log10(lux) + 1, 0xfffe), 0)));
+
         history.setLastEvent();
-        history.addEntry({ time: history.now(), contact: contact === true ? 0 : 1 });
-        this.log.info(`Set contact to ${contact}`);
+        history.addEntry({ time: history.now(), motion: occupancy.occupied === true ? 0 : 1, lux: lux });
+        this.log.info(`Set motion to ${occupancy.occupied} and lux to ${lux}`);
       },
-      60 * 1000 - 500,
+      60 * 1000 - 400,
     );
   }
 
