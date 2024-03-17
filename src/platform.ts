1aba4db13d999410dcb35683c9bf33db1291ce84
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-17 19:49:03 +0100

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -1,9 +1,13 @@
-import { DeviceTypes, logEndpoint, IlluminanceMeasurement, OccupancySensing } from 'matterbridge';
+import { DeviceTypes, IlluminanceMeasurement, OccupancySensing } from 'matterbridge';
 
 import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform, MatterHistory } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 
 export class EveMotionPlatform extends MatterbridgeAccessoryPlatform {
+  motion: MatterbridgeDevice | undefined;
+  history: MatterHistory | undefined;
+  interval: NodeJS.Timeout | undefined;
+
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
   }
@@ -11,49 +15,55 @@ export class EveMotionPlatform extends MatterbridgeAccessoryPlatform {
   override async onStart(reason?: string) {
     this.log.info('onStart called with reason:', reason ?? 'none');
 
-    const history = new MatterHistory(this.log, 'Eve motion', { filePath: this.matterbridge.matterbridgeDirectory });
+    this.history = new MatterHistory(this.log, 'Eve motion', { filePath: this.matterbridge.matterbridgeDirectory });
 
-    const motion = new MatterbridgeDevice(DeviceTypes.OCCUPANCY_SENSOR);
-    motion.createDefaultIdentifyClusterServer();
-    motion.createDefaultBasicInformationClusterServer('Eve motion', '0x85483499', 4874, 'Eve Systems', 89, 'Eve Motion 20EBY9901', 6650, '3.2.1');
-    motion.createDefaultOccupancySensingClusterServer();
+    this.motion = new MatterbridgeDevice(DeviceTypes.OCCUPANCY_SENSOR);
+    this.motion.createDefaultIdentifyClusterServer();
+    this.motion.createDefaultBasicInformationClusterServer('Eve motion', '0x85483499', 4874, 'Eve Systems', 89, 'Eve Motion 20EBY9901', 6650, '3.2.1');
+    this.motion.createDefaultOccupancySensingClusterServer();
 
-    motion.addDeviceType(DeviceTypes.LIGHT_SENSOR);
-    motion.createDefaultIlluminanceMeasurementClusterServer();
+    this.motion.addDeviceType(DeviceTypes.LIGHT_SENSOR);
+    this.motion.createDefaultIlluminanceMeasurementClusterServer();
 
-    motion.createDefaultPowerSourceReplaceableBatteryClusterServer();
-    motion.createDefaultPowerSourceConfigurationClusterServer(1);
+    this.motion.createDefaultPowerSourceReplaceableBatteryClusterServer();
+    this.motion.createDefaultPowerSourceConfigurationClusterServer(1);
 
     // Add the EveHistory cluster to the device as last cluster!
-    motion.createMotionEveHistoryClusterServer(history, this.log);
-    history.autoPilot(motion);
+    this.motion.createMotionEveHistoryClusterServer(this.history, this.log);
+    this.history.autoPilot(this.motion);
 
-    await this.registerDevice(motion);
+    await this.registerDevice(this.motion);
 
-    motion.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
+    this.motion.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
       this.log.warn(`Command identify called identifyTime:${identifyTime}`);
-      logEndpoint(motion);
-      history.logHistory(false);
+      this.history?.logHistory(false);
     });
+  }
 
-    setInterval(
+  override async onConfigure() {
+    this.log.info('onConfigure called');
+
+    this.interval = setInterval(
       () => {
-        const occupancy = motion.getClusterServerById(OccupancySensing.Cluster.id)?.getOccupancyAttribute();
+        if (!this.motion || !this.history) return;
+        const occupancy = this.motion.getClusterServerById(OccupancySensing.Cluster.id)?.getOccupancyAttribute();
         if (!occupancy) return;
         occupancy.occupied = !occupancy.occupied;
-        const lux = history.getFakeLevel(0, 1000, 0);
-        motion.getClusterServerById(OccupancySensing.Cluster.id)?.setOccupancyAttribute(occupancy);
-        motion.getClusterServerById(IlluminanceMeasurement.Cluster.id)?.setMeasuredValueAttribute(Math.round(Math.max(Math.min(10000 * Math.log10(lux) + 1, 0xfffe), 0)));
+        const lux = this.history.getFakeLevel(0, 1000, 0);
+        this.motion.getClusterServerById(OccupancySensing.Cluster.id)?.setOccupancyAttribute(occupancy);
+        this.motion.getClusterServerById(IlluminanceMeasurement.Cluster.id)?.setMeasuredValueAttribute(Math.round(Math.max(Math.min(10000 * Math.log10(lux) + 1, 0xfffe), 0)));
 
-        history.setLastEvent();
-        history.addEntry({ time: history.now(), motion: occupancy.occupied === true ? 0 : 1, lux });
+        this.history.setLastEvent();
+        this.history.addEntry({ time: this.history.now(), motion: occupancy.occupied === true ? 0 : 1, lux });
         this.log.info(`Set motion to ${occupancy.occupied} and lux to ${lux}`);
       },
-      60 * 1000 - 400,
+      60 * 1000 + 200,
     );
   }
 
   override async onShutdown(reason?: string) {
     this.log.info('onShutdown called with reason:', reason ?? 'none');
+
+    clearInterval(this.interval);
   }
 }
